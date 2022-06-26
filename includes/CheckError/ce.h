313262102ab888e93b2bbb052a02f5191c9e897f
daniil <dddmitriev@hex.team>
2022-06-26 11:53:02 +0300

    Add simple check error system

--- includes/CheckError/ce.h
+++ includes/CheckError/ce.h
@@ -1,3 +1,18 @@
 #pragma once
 
+// CE_ prefix means all macro is for ce() function
+// First 5 (1-5) err codes are reserved for support legacy (or lazy)
+
+// use 0 as OK status code
+#define CE_OK 0
+
+// calls when args od function are unaviable
+#define CE_WRONG_INPUT 6
+
+// calls when pointer, with whom to work is non init
+#define CE_NULL_PTR 7
+
+// calls when has calculation error
+#define CE_CALC 8
+
 char ce(char err_code);
--- src/CheckError/ce.c
+++ src/CheckError/ce.c
@@ -2,11 +2,31 @@
 
 #include <stdio.h>
 
-char ce(char error_code) {
-    if (error_code == 0) {
-        return 1;
-    }
-    printf("Some error [%d]", error_code);
+// this defines are pretty locked up in this source file
+#define true 1
+#define false 0
 
-    return 0;
+char ce(char error_code) {
+    if (error_code == CE_OK) {
+        return true;
+    }
+    switch (error_code) {
+    case CE_WRONG_INPUT:
+        printf("[ERR] Wring input args");
+        return false;
+
+    case CE_NULL_PTR:
+        printf("[ERR] Working with uninitialized ptr");
+        return false;
+
+    case CE_CALC:
+        printf("[ERR] Detected invalid calculation");
+        return false;
+
+    default:
+        printf("[ERR] Some error with code [%d]", error_code);
+        return false;
+    }
+    //
+    return false;
 }
